b15ece4442070182abaf06aec653aefab046fd1e
Aras Pranckevicius <aras@unity3d.com>
2018-04-16 21:38:23 +0300

    C#: add "regular" .NET 4.6 project too (TestCsRegular).
    I would have used one of them "multi-targeting" thingies in one .csproj, but VSMac does not support them, i.e. no UI to pick which one to target from the IDE.
    
    So a separate .csproj it is. Which of course is complicated in something called project.json (whatever that is, I've no idea), and needs a custom <BaseIntermediateOutputPath> tag in the new .csproj. See https://github.com/NuGet/Home/issues/5126
    
    Computers were a mistake!
    
    Anyhoo, numbers in Mray/s on Mac (see issue #3):
    - dotnet (2.1.4): 13.0
    - mono (5.8.1): 5.5
    - mono -O=float32: 6.0
    - mono --llvm: 7.8
    - mono --llvm -O=float32: 10.5
    - MONO_INLINELIMIT=100 mono --llvm -O=float32: 12.7

--- Cs/Maths.cs
+++ Cs/Maths.cs
@@ -2,6 +2,22 @@ using System;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
+// MathF class for floats only appeared in NET Core; have a manual workaround
+// implementation using System.Math for regular .NET versions.
+#if NET46
+public static class MathF
+{
+    public static float Sqrt(float v) { return (float)Math.Sqrt(v); }
+    public static float Abs(float v) { return (float)Math.Abs(v); }
+    public static float Pow(float a, float b) { return (float)Math.Pow(a, b); }
+    public static float Sin(float v) { return (float)Math.Sin(v); }
+    public static float Cos(float v) { return (float)Math.Cos(v); }
+    public static float Tan(float v) { return (float)Math.Tan(v); }
+    public static float Max(float a, float b) { return (float) Math.Max(a, b); }
+}
+#endif // #if NET46
+
+
 public struct float3
 {
     public float x, y, z;
